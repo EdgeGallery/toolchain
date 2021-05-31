8cab8ac8e6a8d668242619b3da3562f5d748227b
luoxiaoyun <hw.luoxiaoyun@huawei.com>
2021-05-31 15:21:03 +0800

    【AppStore】APPD转换器 https://gitee.com/OSDT/dashboard/issues?id=I2E166

---
+++ appdtranstool/src/main/java/org/edgegallery/tool/appdtrans/controller/VmController.java
@@ -0,0 +1,87 @@
+/*
+ *  Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.edgegallery.tool.appdtrans.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.edgegallery.tool.appdtrans.controller.dto.request.Chunk;
+import org.edgegallery.tool.appdtrans.controller.dto.response.ErrorRespDto;
+import org.edgegallery.tool.appdtrans.service.VmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "appdtranstool api system")
+@Validated
+@RequestMapping("/mec/appdtranstool/v1/vm")
+@RestController
+public class VmController {
+    @Autowired
+    private VmService vmService;
+
+    /**
+     * get appd templates types.
+     *
+     */
+    @GetMapping(value = "/templates", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK", response = ResponseEntity.class),
+        @ApiResponse(code = 400, message = "Bad Request", response = ErrorRespDto.class)
+    })
+    public ResponseEntity<List<String>> getVmTemplates() {
+        return ResponseEntity.ok(vmService.getVmTemplates());
+    }
+
+    /**
+     * upload image.
+     */
+    @ApiOperation(value = "upload image", response = ResponseEntity.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK", response = ResponseEntity.class),
+        @ApiResponse(code = 400, message = "Bad Request", response = ErrorRespDto.class)
+    })
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ResponseEntity<String> uploadImage(HttpServletRequest request, Chunk chunk) throws Exception {
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+        return vmService.uploadImage(isMultipart,chunk);
+    }
+
+    /**
+     * merge image.
+     */
+    @ApiOperation(value = "merge image", response = ResponseEntity.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK", response = ResponseEntity.class),
+        @ApiResponse(code = 400, message = "Bad Request", response = ErrorRespDto.class)
+    })
+    @RequestMapping(value = "/apps/merge", method = RequestMethod.GET)
+    public ResponseEntity<String> merge(@RequestParam(value = "fileName") String fileName,
+        @RequestParam(value = "guid") String guid) throws Exception {
+        return vmService.merge(fileName,guid);
+    }
+}
